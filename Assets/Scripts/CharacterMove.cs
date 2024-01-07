9b6b5a84bd964bbcb68227fabdc990ab760468ac
Thaisond98 <147731298+dangthaison98@users.noreply.github.com>
2024-01-07 21:21:04 +0700

    Add map

--- Assets/Scripts/CharacterMove.cs
+++ Assets/Scripts/CharacterMove.cs
@@ -4,10 +4,36 @@ using UnityEngine;
 
 public class CharacterMove : MonoBehaviour
 {
+    public Transform checkWallPoint;
+    public Transform checkGroundPoint;
 
+    private Rigidbody2D rb;
 
-    private void Update()
+    private void Awake()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (GroundCheck())
+        {
+            rb.velocity = transform.right * 50 * Time.fixedDeltaTime;
+        }
+
+        if(WallCheck())
+        {
+            transform.eulerAngles = new Vector3(0, transform.eulerAngles.y + 180, 0);
+        }
+    }
+
+
+    public bool WallCheck()
+    {
+        return Physics2D.Raycast(checkWallPoint.position, Vector2.right, 0.01f, LayerMask.GetMask("Ground"));
+    }
+    public bool GroundCheck()
+    {
+        return Physics2D.OverlapCircle(checkGroundPoint.position, 0.1f, LayerMask.GetMask("Ground"));
     }
 }
--- Assets/Scripts/Window.cs
+++ Assets/Scripts/Window.cs
@@ -2,11 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class Window : MonoBehaviour
 {
     private Camera _camera;
     private Vector3 standPoint;
+
     private void Awake()
     {
         _camera = Camera.main;
@@ -20,7 +22,7 @@ public class Window : MonoBehaviour
     private void OnMouseDrag()
     {
         Vector2 newPos = _camera.ScreenToWorldPoint(Input.mousePosition);
-        transform.position = Vector3.Lerp(transform.position, newPos, 15 * Time.deltaTime);
+        transform.position = Vector3.MoveTowards(transform.position, newPos, 50 * Time.deltaTime);
     }
     private void OnMouseUp()
     {
@@ -32,7 +34,7 @@ public class Window : MonoBehaviour
     {
         while (transform.position != standPoint)
         {
-            transform.position = Vector3.Lerp(transform.position, standPoint, 15 * Time.deltaTime);
+            transform.position = Vector3.MoveTowards(transform.position, standPoint, 50 * Time.deltaTime);
             yield return null;
         }
     }
